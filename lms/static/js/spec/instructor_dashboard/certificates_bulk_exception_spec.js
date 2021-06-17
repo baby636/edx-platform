48ed633259f447586d2afd892042ece09f025c3e
Justin Hynes <JHynes@edx.org>
2021-06-17 08:29:58 -0400

    Rename instructor dashboard pieces that refer to "whitelist" to "allowlist" wherever possible

--- lms/static/js/spec/instructor_dashboard/certificates_bulk_exception_spec.js
+++ lms/static/js/spec/instructor_dashboard/certificates_bulk_exception_spec.js
@@ -1,28 +1,28 @@
 define([
     'jquery',
-    'js/certificates/views/certificate_bulk_whitelist'
+    'js/certificates/views/certificate_bulk_allowlist'
 ],
-    function($, CertificateBulkWhiteListView) {
+    function($, CertificateBulkAllowlistView) {
         'use strict';
         describe('certificate bulk exceptions generation', function() {
             var certificate_bulk_exception_url = 'test/url/';
             var SELECTORS = {
                 upload_csv_button: '.upload-csv-button',
-                bulk_white_list_exception_form: 'form#bulk-white-list-exception-form',
+                bulk_allowlist_exception_form: 'form#bulk-allowlist-exception-form',
                 bulk_exception_results: '.bulk-exception-results'
             };
             beforeEach(function() {
                 setFixtures();
                 var fixture = readFixtures(
-                    'templates/instructor/instructor_dashboard_2/certificate-bulk-white-list.underscore'
+                    'templates/instructor/instructor_dashboard_2/certificate-bulk-allowlist.underscore'
                 );
 
                 setFixtures(
-                    "<script type='text/template' id='certificate-bulk-white-list-tpl'>" + fixture + '</script>' +
-                    "<div class='bulk-white-list-exception'></div>"
+                    "<script type='text/template' id='certificate-bulk-allowlist-tpl'>" + fixture + '</script>' +
+                    "<div class='bulk-allowlist-exception'></div>"
                 );
 
-                this.view = new CertificateBulkWhiteListView({
+                this.view = new CertificateBulkAllowlistView({
                     bulk_exception_url: certificate_bulk_exception_url
                 });
                 this.view.render();
@@ -41,7 +41,7 @@ define([
                     };
                 });
                 submitCallback = jasmine.createSpy().and.returnValue();
-                this.view.$el.find(SELECTORS.bulk_white_list_exception_form).submit(submitCallback);
+                this.view.$el.find(SELECTORS.bulk_allowlist_exception_form).submit(submitCallback);
                 this.view.$el.find(SELECTORS.upload_csv_button).click();
                 expect($(SELECTORS.bulk_exception_results).text()).toContain('1 learner is successfully added to the ' +
                     'exception list');
@@ -60,7 +60,7 @@ define([
                     };
                 });
                 submitCallback = jasmine.createSpy().and.returnValue();
-                this.view.$el.find(SELECTORS.bulk_white_list_exception_form).submit(submitCallback);
+                this.view.$el.find(SELECTORS.bulk_allowlist_exception_form).submit(submitCallback);
                 this.view.$el.find(SELECTORS.upload_csv_button).click();
                 expect($(SELECTORS.bulk_exception_results).text()).toContain('File is not attached.');
             });
@@ -73,7 +73,7 @@ define([
                         row_errors: {
                             data_format_error: ['user 1 in row# 1'],
                             user_not_exist: ['user 2 in row# 2'],
-                            user_already_white_listed: ['user 3 in row# 3'],
+                            user_already_allowisted: ['user 3 in row# 3'],
                             user_not_enrolled: ['user 4 in row# 4'],
                             user_on_certificate_invalidation_list: ['user 5 in row# 5']
                         },
@@ -84,7 +84,7 @@ define([
                     };
                 });
                 submitCallback = jasmine.createSpy().and.returnValue();
-                this.view.$el.find(SELECTORS.bulk_white_list_exception_form).submit(submitCallback);
+                this.view.$el.find(SELECTORS.bulk_allowlist_exception_form).submit(submitCallback);
                 this.view.$el.find(SELECTORS.upload_csv_button).click();
                 expect($(SELECTORS.bulk_exception_results).text()).toContain('1 record is not in correct format');
                 expect($(SELECTORS.bulk_exception_results).text()).toContain('1 learner does not exist in LMS');
@@ -103,7 +103,7 @@ define([
                         row_errors: {
                             data_format_error: ['user 1 in row# 1', 'user 1 in row# 1'],
                             user_not_exist: ['user 2 in row# 2', 'user 2 in row# 2'],
-                            user_already_white_listed: ['user 3 in row# 3', 'user 3 in row# 3'],
+                            user_already_allowlisted: ['user 3 in row# 3', 'user 3 in row# 3'],
                             user_not_enrolled: ['user 4 in row# 4', 'user 4 in row# 4'],
                             user_on_certificate_invalidation_list: ['user 5 in row# 5', 'user 5 in row# 5']
                         },
@@ -114,7 +114,7 @@ define([
                     };
                 });
                 submitCallback = jasmine.createSpy().and.returnValue();
-                this.view.$el.find(SELECTORS.bulk_white_list_exception_form).submit(submitCallback);
+                this.view.$el.find(SELECTORS.bulk_allowlist_exception_form).submit(submitCallback);
                 this.view.$el.find(SELECTORS.upload_csv_button).click();
                 expect($(SELECTORS.bulk_exception_results).text()).toContain('2 records are not in correct format');
                 expect($(SELECTORS.bulk_exception_results).text()).toContain('2 learners do not exist in LMS');
@@ -138,7 +138,7 @@ define([
                     };
                 });
                 submitCallback = jasmine.createSpy().and.returnValue();
-                this.view.$el.find(SELECTORS.bulk_white_list_exception_form).submit(submitCallback);
+                this.view.$el.find(SELECTORS.bulk_allowlist_exception_form).submit(submitCallback);
                 this.view.$el.find(SELECTORS.upload_csv_button).click();
                 expect(this.view.$el.find('div.message > .successfully-added')).toBeHidden();
                 this.view.$el.find('.arrow#successfully-added').trigger('click');
